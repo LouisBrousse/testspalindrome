2e2ff7d075e3f5c9fa95e5a53bf0d028ef9c6154
LouisBrousse <brousselouis@hotmail.com>
2025-06-11 09:48:11 +0200

    premier jour

--- src/main.py
+++ src/main.py
@@ -1,32 +1,61 @@
+from datetime import datetime, time
+
 class Ohce:
     
-    def ditbonjour():
-        print("Bonjour")
+    def __parlefr__():
+        bonjour = "Bonjour!"
+        bonsoir = "Bonsoir!"
+        aurevoir = "Au revoir"
+        return(bonjour, bonsoir, aurevoir)
     
-    # def demandechaine():
-    #     chaine = input("Entrez une chaîne de caractères : ")
-    #     return chaine
-
-
-    def palindrome(chaine):
-        Ohce.bonjour
-        chaine = 
-        # Inverser la chaîne de caractères
-        chaine_inverse = chaine[::-1]
-        print(chaine_inverse)
+    def __parleen__():
+        bonjour = "Hello!"
+        bonsoir = "Good evening!"
+        aurevoir = "Good by!"
+        return(bonjour, bonsoir, aurevoir)
     
     
+    def __quelheureestil__():
+        return datetime.now()
     
     
-    def biendit(chaine):
+    def __biendit__(chaine):
         if chaine == chaine[::-1]:
-            return "Bien dit"
+            print ("Bien dit!")
+        else :
+            print (chaine[::-1])
 
-    def au_revoir():
-        return "Au revoir"
+    def palindrome(heure_fn=None, langue=None):
+        # Heure
+        if heure_fn is None:
+            heure_fn = Ohce.__quelheureestil__
+        heure = heure_fn()
+
+        # Langue
+        if langue == "en":
+            bonjour, bonsoir, aurevoir = Ohce.__parleen__()
+        elif langue == None or "fr":
+            bonjour, bonsoir, aurevoir = Ohce.__parlefr__()
+        
+
+        if heure.time() < time(18, 0):
+            print(bonjour)
+        else:
+            print(bonsoir)
+                
+        chaine = input("> ")
+        
+        Ohce.__biendit__(chaine)
+       
+        print(aurevoir)
+    
+    
     
     def detecteur(chaine):
         if chaine == chaine[::-1]:
             return Ohce.biendit(chaine)
         else:
             return Ohce.palindrome(chaine)
+
+if __name__ == "__main__":
+    Ohce.palindrome()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,103 @@
 from src.main import Ohce
-def test_nominal():
-    # ETANT DONNE une chainbe de caractères
-    chaine = "caracteres"
-    # QUAND on l'envoie au détecteur de palindrome
-    chaine_inverse = Ohce.palindrome(chaine)
-    # ALORS IL RENVOI la chaîne à l'envers
-    assert chaine_inverse == "seretcarac"
+from datetime import datetime
+import builtins
 
-def test_bonjour():
+def test_nominal(monkeypatch, capfd):
     # ETANT DONNE une chainbe de caractères
+    # On simule l'entrée de l'utilisateur
+    monkeypatch.setattr(builtins, 'input', lambda _: "test")
     # QUAND on l'envoie au détecteur de palindrome
-    # ALORS IL PRINT "Bonjour"
-    # ET il renvoi la chaîne à l'envers
+    result = Ohce.palindrome()
+    # ALORS IL RENVOI la chaîne à l'envers
+    out, _ = capfd.readouterr()
+    assert "tset" in out
+
+def test_palindrome(monkeypatch, capfd):
+    # ETANT DONNE une chainbe de caractères
+    # On simule l'entrée de l'utilisateur
+    monkeypatch.setattr(builtins, 'input', lambda _: "kayak")
+    # QUAND on l'envoie au détecteur de palindrome
+    result = Ohce.palindrome()
+    # ALORS IL RENVOI la chaîne à l'envers
+    out, _ = capfd.readouterr()
+    assert "Bien dit" in out
+
+def test_bonjour(monkeypatch, capfd):
+    # ETANT DONNE un démarage du programme avant 18h
+    # Et on simule l'entrée de l'utilisateur
+    fake_time = lambda: datetime(2025, 1, 1, 9, 0, 0)
+    monkeypatch.setattr(builtins, "input", lambda _: "palindrome")
+    # QUAND on l'envoie au détecteur de palindrome
+    result = Ohce.palindrome(fake_time)
+    # ALORS il renvoi "Bonjour!"
+    out, _ = capfd.readouterr()
+    assert "Bonjour!" in out
+
+def test_bonjour2(monkeypatch, capfd):
+    # ETANT DONNE un démarage du programme avant 18h
+    # Et on simule l'entrée de l'utilisateur
+    fake_time = lambda: datetime(2025, 1, 1, 9, 17, 59)
+    monkeypatch.setattr(builtins, "input", lambda _: "palindrome")
+    # QUAND on l'envoie au détecteur de palindrome
+    result = Ohce.palindrome(fake_time)
+    # ALORS il renvoi "Bonjour!"
+    out, _ = capfd.readouterr()
+    assert "Bonjour!" in out
+
+def test_bonsoir(monkeypatch, capfd):
+    # ETANT DONNE un démarage du programme à 18h
+    # Et on simule l'entrée de l'utilisateur
+    fake_time = lambda: datetime(2023, 10, 1, 18, 0)
+    monkeypatch.setattr(builtins, "input", lambda _: "palindrome")
+    # QUAND on l'envoie au détecteur de palindrome
+    result = Ohce.palindrome(fake_time)
+    # ALORS il renvoi "Bonsoir!"
+    out, _ = capfd.readouterr()
+    assert "Bonsoir!" in out
+
+def test_bonsoir2(monkeypatch, capfd):
+    # ETANT DONNE un démarage du programme à 18h
+    # Et on simule l'entrée de l'utilisateur
+    fake_time = lambda: datetime(2023, 10, 1, 23, 59)
+    monkeypatch.setattr(builtins, "input", lambda _: "palindrome")
+    # QUAND on l'envoie au détecteur de palindrome
+    result = Ohce.palindrome(fake_time)
+    # ALORS il renvoi "Bonsoir!"
+    out, _ = capfd.readouterr()
+    assert "Bonsoir!" in out
+
+def test_biendit(monkeypatch, capfd):
+    # ETANT DONNE une chainbe de caractères
+    # On simule l'entrée de l'utilisateur
+    monkeypatch.setattr(builtins, 'input', lambda _: "kayak")
+    # QUAND on l'envoie au détecteur de palindrome
+    result = Ohce.palindrome()
+    # ALORS IL RENVOI la chaîne à l'envers
+    out, _ = capfd.readouterr()
+    assert "Bien dit!" in out
+
+def test_bonjouren(monkeypatch, capfd):
+    # ETANT DONNE un démarage du programme avant 18h
+    # Et on simule l'entrée de l'utilisateur
+    fake_time = lambda: datetime(2025, 1, 1, 9, 0, 0)
+    monkeypatch.setattr(builtins, "input", lambda _: "palindrome")
+    # QUAND on l'envoie au détecteur de palindrome
+    result = Ohce.palindrome(fake_time, langue="en")
+    # ALORS il renvoi "Bonjour!" en Anglais
+    out, _ = capfd.readouterr()
+    assert "Hello!" in out
+
+def test_bonsoiren(monkeypatch, capfd):
+    # ETANT DONNE un démarage du programme à 18h
+    # Et on simule l'entrée de l'utilisateur
+    fake_time = lambda: datetime(2023, 10, 1, 18, 0)
+    monkeypatch.setattr(builtins, "input", lambda _: "palindrome")
+    # QUAND on l'envoie au détecteur de palindrome
+    result = Ohce.palindrome(fake_time, langue="en")
+    # ALORS il renvoi "Bonsoir!" en Anglais
+    out, _ = capfd.readouterr()
+    assert "Good evening!" in out
+
+
+
+    # assert trop laxe, lisibilité des tests, cas de tests sur bonjour aurevoir.
