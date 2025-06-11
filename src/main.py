5fdbb0cbd0d68e9f7b6aaf22b7423645c9c80fc4
LouisBrousse <brousselouis@hotmail.com>
2025-06-11 10:31:34 +0200

    correction initial

--- src/main.py
+++ src/main.py
@@ -1,29 +1,34 @@
 class Ohce:
     
-    def ditbonjour():
-        print("Bonjour")
-    
-    # def demandechaine():
-    #     chaine = input("Entrez une chaîne de caractères : ")
-    #     return chaine
+    def ditbonjour(self):
+        return"Bonjour \n"
 
-
-    def palindrome(chaine):
-        Ohce.bonjour
-        chaine = 
-        # Inverser la chaîne de caractères
-        chaine_inverse = chaine[::-1]
-        print(chaine_inverse)
+    def au_revoir(self):
+        return 
     
     
-    
-    
-    def biendit(chaine):
+    def biendit(self, chaine):
         if chaine == chaine[::-1]:
-            return "Bien dit"
+            return chaine + "Bien dit \n"
+        else :
+            return chaine[::-1]
 
-    def au_revoir():
-        return "Au revoir"
+    def palindrome(self, chaine):
+        reponse=self.ditbonjour()    # def demandechaine():
+
+        # Inverser la chaîne de caractères
+        
+        # if chaine == chaine_inverse:
+            # return chaine_inverse + self.biendit
+        reponse += self.biendit(chaine)
+        
+        reponse += self.au_revoir()
+
+        return reponse
+    
+
+
+
     
     def detecteur(chaine):
         if chaine == chaine[::-1]:
