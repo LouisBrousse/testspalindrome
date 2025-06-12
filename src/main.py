dcaef44c60d7024b72cf1a9aee71bb6677d2fc8f
LouisBrousse <brousselouis@hotmail.com>
2025-06-12 17:22:00 +0200

    [GREEN] Premier Test OK

--- src/main.py
+++ src/main.py
@@ -1,36 +1,9 @@
-import datetime
+from src.DetecteurPalindrome import DetecteurPalindrome
 
 class Ohce:
-    def __init__(self, heure=None, langue="fr"):
-        if heure is None:
-            self.heure = datetime.datetime.now().time()
-        else:
-            self.heure = datetime.datetime.strptime(heure, "%H:%M").time()
-        self.langue = langue
-    
-    def ditbonjour(self):
-        limite = datetime.time(18, 0)
 
-        if self.heure >= limite:
-            return "Bonsoir \n"
-        else:
-            return "Bonjour \n"
+    def decterpalindrome(chaine):
+        DetecteurPalindrome(chaine).miroir()
+        toto = DetecteurPalindrome(chaine).miroir()
+        return toto
 
-    def au_revoir(self):
-        return "Au revoir"
-    
-    def miroir(self, chaine):
-        if chaine == chaine[::-1]:
-            return chaine + "\n"+ "Bien dit \n"
-        else :
-            return chaine[::-1] + " \n"
-    
-    def palindrome(self, chaine):
-        reponse=self.ditbonjour()  
-
-        reponse += self.miroir(chaine)
-        
-        reponse += self.au_revoir()
-
-        return reponse
-    
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,39 +4,38 @@ def test_nominal():
     # ETANT DONNE une chainbe de caractères
     chaine = "caracteres"
     # QUAND on l'envoie au détecteur de palindrome
-    result = Ohce().palindrome(chaine)
+    result = Ohce.decterpalindrome(chaine)
     # ALORS IL RENVOI la chaîne à l'envers
     assert "seretcarac \n" in result
 
-def test_bonjour():
-    # ETANT DONNE une chainbe de caractères
-    chaine = "caracteres"
-    # QUAND on l'envoie au détecteur de palindrome
-    result = Ohce().palindrome(chaine)
-    # ALORS IL RENVOI bonjour au début
-    assert result.startswith("Bonjour \n")
+# def miroir(self):
+#         miroir = self.chaine[::-1]
+#         if self.chaine == miroir:
+#             return miroir + " Bien dit \n"
+#         else:
+#             return miroir + " \n" 
 
-def test_aurevoir():
-    # ETANT DONNE une chainbe de caractères
-    chaine = "caracteres"
-    # QUAND on l'envoie au détecteur de palindrome
-    result = Ohce().palindrome(chaine)
-    # ALORS IL RENVOI Aurevoir a la fin
-    assert result.endswith("Au revoir")
+# def test_aurevoir():
+#     # ETANT DONNE une chainbe de caractères
+#     chaine = "caracteres"
+#     # QUAND on l'envoie au détecteur de palindrome
+#     result = Ohce().palindrome(chaine)
+#     # ALORS IL RENVOI Aurevoir a la fin
+#     assert result.endswith("Au revoir")
 
-def test_kayak():
-    # ETANT DONNE une chainbe de caractères qui est un palindrome
-    chaine = "kayak"
-    # QUAND on l'envoie au détecteur de palindrome
-    result = Ohce().palindrome(chaine)
-    # ALORS IL RENVOI Bien dit après la chaine
-    assert "Bien dit \n" 
+# def test_kayak():
+#     # ETANT DONNE une chainbe de caractères qui est un palindrome
+#     chaine = "kayak"
+#     # QUAND on l'envoie au détecteur de palindrome
+#     result = Ohce().palindrome(chaine)
+#     # ALORS IL RENVOI Bien dit après la chaine
+#     assert "Bien dit \n" 
 
 
-def test_pasbiendit():
-    # ETANT DONNE une chainbe de caractères
-    chaine = "caracteres"
-    # QUAND on l'envoie au détecteur de palindrome
-    result = Ohce().palindrome(chaine)
-    # ALORS IL RENVOI la chaîne à l'envers
-    assert "Bien dit \n" not in result
+# def test_pasbiendit():
+#     # ETANT DONNE une chainbe de caractères
+#     chaine = "caracteres"
+#     # QUAND on l'envoie au détecteur de palindrome
+#     result = Ohce().palindrome(chaine)
+#     # ALORS IL RENVOI la chaîne à l'envers
+#     assert "Bien dit \n" not in result
