55e86ed983b9ba8fa2c704bb51786ebffcd793f3
LouisBrousse <brousselouis@hotmail.com>
2025-06-26 20:09:27 +0200

    [GREEN] initial test2 Code remanié suite à correction de l'ancien

--- src/main.py
+++ src/main.py
@@ -1,20 +1,12 @@
-from src.DetecteurPalindrome import DetecteurPalindrome
+from DetecteurPalindrome import DetecteurPalindrome
 
 def main():
 
-    detecteur = DetecteurPalindrome()
-
     while True:
-        try:
+       
             entree = input("> ")
-            if not entree:
-                raise ValueError("Aucune saisie")
-            print(entree + " => " + detecteur.miroir(entree))
-        except (EOFError, KeyboardInterrupt):
-            print("\n" + "Au revoir !")
-            break
-        except Exception as e:
-            print(f"Erreur : {e}")
+            print(entree + " => " + DetecteurPalindrome.miroir(entree))
+         
 
 if __name__ == "__main__":
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,9 +2,7 @@ from src.DetecteurPalindrome import DetecteurPalindrome
 def test_nominal():
     # ETANT DONNE une chaine de caractères
     chaine = "caracteres"
-    
     # QUAND on l'envoie au détecteur de palindrome
-    dp = DetecteurPalindrome(chaine)
-    result = dp.miroir()
+    result = DetecteurPalindrome.miroir(chaine)
     # ALORS IL RENVOI la chaîne à l'envers
-    assert "seretcarac \n" in result
+    assert "seretcarac" in result
