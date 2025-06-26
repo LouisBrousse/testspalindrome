db6abdbd317ee935e057b30883f097774c57000d
LouisBrousse <brousselouis@hotmail.com>
2025-06-26 20:19:50 +0200

    [GREEN]Bien dit

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,5 +12,22 @@ def test_palindrome():
     chaine = "kayak"
     # QUAND on l'envoie au détecteur de palindrome
     result = DetecteurPalindrome.miroir(chaine)
-    # ALORS IL RENVOI la chaîne à l'envers avec un message
-    assert "kayak Bien dit!" in result
+    # ALORS IL RENVOI la chaîne à l'envers
+    assert "kayak" in result
+
+def test_bien_dit():
+    # ETANT DONNE une chaine de caractères palindrome
+    chaine = "kayak"
+    # QUAND on l'envoie au détecteur de palindrome
+    result = DetecteurPalindrome.miroir(chaine)
+    # ALORS IL RENVOI la chaîne à l'envers avec "Bien dit!"
+    assert "Bien dit!" in result
+
+def test_non_bien_dit():
+    # ETANT DONNE une chaine de caractères non palindrome
+    chaine = "hello"
+    # QUAND on l'envoie au détecteur de palindrome
+    result = DetecteurPalindrome.miroir(chaine)
+    # ALORS IL RENVOI la chaîne à l'envers sans "Bien dit!"
+    assert "Bien dit!" not in result
+    assert "olleh" in result
