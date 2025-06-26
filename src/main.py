df6c46c4ab5867f6a31d6bd169a410f1791a947c
LouisBrousse <brousselouis@hotmail.com>
2025-06-26 18:50:43 +0200

    [RED] initial test

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
 from src.DetecteurPalindrome import DetecteurPalindrome
 
-
-detecteur = new DetecteurPalindrome("caracteres")
 def main():
-    chaine = "caracteres"
-    result = detecteur.miroir()
-    print(result)
+
+    detecteur = DetecteurPalindrome()
+
+    while True:
+        try:
+            entree = input("> ")
+            if not entree:
+                raise ValueError("Aucune saisie")
+            print(entree + " => " + detecteur.miroir(entree))
+        except (EOFError, KeyboardInterrupt):
+            print("\n" + "Au revoir !")
+            break
+        except Exception as e:
+            print(f"Erreur : {e}")
+
+if __name__ == "__main__":
+    main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,9 @@
-from src.main import Ohce
-
+from src.DetecteurPalindrome import DetecteurPalindrome 
 def test_nominal():
-    # ETANT DONNE une chainbe de caractères
+    # ETANT DONNE une chaine de caractères
     chaine = "caracteres"
+    
     # QUAND on l'envoie au détecteur de palindrome
-    result = Ohce.decterpalindrome(chaine)
+    result = DetecteurPalindrome.miroir(chaine)
     # ALORS IL RENVOI la chaîne à l'envers
     assert "seretcarac \n" in result
