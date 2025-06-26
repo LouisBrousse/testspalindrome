5c55e90428264adb7ce65f0433855b2b4397a1de
LouisBrousse <brousselouis@hotmail.com>
2025-06-26 20:11:25 +0200

    [RED]Diversification des chaines

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,3 +6,11 @@ def test_nominal():
     result = DetecteurPalindrome.miroir(chaine)
     # ALORS IL RENVOI la chaîne à l'envers
     assert "seretcarac" in result
+
+def test_palindrome():
+    # ETANT DONNE une chaine de caractères palindrome
+    chaine = "kayak"
+    # QUAND on l'envoie au détecteur de palindrome
+    result = DetecteurPalindrome.miroir(chaine)
+    # ALORS IL RENVOI la chaîne à l'envers avec un message
+    assert "kayak Bien dit!" in result
